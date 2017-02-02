158e4297ce59ce2242cfa614a71fcc687bf59e5b
Alexandr <avbubnov@mail.ru>
2017-02-02 15:36:52 +0300

    commit7

--- testgithub/test1/src/ru/spb/main/test/TestCore.java
+++ testgithub/test1/src/ru/spb/main/test/TestCore.java
@@ -5,8 +5,9 @@ public class TestCore {
 	public static void main(String[] args) {
 		
 		System.out.println("TestCore:main:001");
-		
-		System.out.println("TestCore:main:00121");
+		int i=1;
+		i=i++;
+		System.out.println("TestCore:main:002");
 	}
 
 }
